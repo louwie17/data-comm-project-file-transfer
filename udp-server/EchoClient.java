e2d2d1a396e0a0fc3d9d3eeabb6630eebefa3709
var1 <var2>
2012-10-30 00:01:43 -0300

    Got it working so that file are the same when transferred took some modefying debug statements are still in place

--- udp-server/EchoClient.java
+++ udp-server/EchoClient.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class EchoClient
 {
 
-  private static final int BUFFER_SIZE = 512;
+  private static final int BUFFER = 1024;
   private static final int PORT = 4444;
 
 
@@ -28,95 +28,95 @@ public class EchoClient
       ip = scan.nextLine();
       if (ip.equals("quit"))
         System.exit(0);
-      //socket.connect(InetAddress.getByName(ip), PORT);
-      byte[] buffer = new byte[BUFFER_SIZE];
-      String cmd = "hi";
-      DatagramPacket connect = new DatagramPacket(cmd.getBytes(), 
-          cmd.getBytes().length, 
-          InetAddress.getByName(ip), PORT);
-      socket.send(connect);
-      System.out.println("send request"+connect.getAddress());
-      DatagramPacket received = new DatagramPacket(buffer, buffer.length);
-      System.out.println("waiting for reply"); 
-      socket.receive(received);
-      String pack = new String(received.getData(), 0, received.getLength());
-      System.out.println(pack); 
-      if (pack.equals("what?"))
+      DatagramPacket connect = null;
+      DatagramPacket received = null; 
+      String cmd = "";
+      byte[] buffer = new byte[BUFFER];
+      try {
+        cmd = "GET_FILE";
+        connect = new DatagramPacket(cmd.getBytes(), 
+            cmd.getBytes().length, 
+            InetAddress.getByName(ip), PORT);
+        socket.send(connect);
+      }
+      catch (IOException e)
       {
-          System.out.println("Connected to: " + received.getAddress());
-      
+        System.out.println("could not connect.");
+        continue;
+      }
+      System.out.println("send request"+connect.getAddress());
+      try {
+        received = new DatagramPacket(buffer, buffer.length);
+        socket.receive(received);
+      }
+      catch (SocketTimeoutException e)
+      {
+        System.out.println("server took to long");
+        continue;
+      }
+      String pack = new String(received.getData(), 0, received.getLength());
+      if (pack.equals("OKGO"))
+      {
           System.out.println("File to transfer: ");
           filename = scan.nextLine();
           if (filename.equals("quit"))
             break;
-          String getf = "GetFile";
-          DatagramPacket askFile = new DatagramPacket(getf.getBytes(),
-              getf.getBytes().length, received.getAddress(), 
-              received.getPort());
-          socket.send(askFile);
+          System.out.println("good to go");
+          DatagramPacket fileName = new DatagramPacket(filename.getBytes(),
+              filename.getBytes().length, InetAddress.getByName(ip),
+              PORT);
+          socket.send(fileName);
+        }
+        else
+          continue;
+
+        socket.receive(received);
+        pack = new String(received.getData(),0,received.getLength());
+        System.out.println(pack);
+        if (pack.startsWith("Error"))
+        {
+          continue;
+        }
+        String[] splitName = filename.split("/");
+        File newFile = new File("local_" + 
+            splitName[splitName.length -1]);
+        if (newFile.exists())
+        {
+          System.out.println("Error: can't write to disk");
+          continue;
+        }
+
+        newFile.createNewFile();
+        Long fileSize = Long.parseLong(pack);
+        FileOutputStream fos = new FileOutputStream(newFile);
+        long total = 0;
+        long totalLeft = fileSize;
+        while (true)
+        {
           socket.receive(received);
-          pack = new String(received.getData(),0,received.getLength());
-          if (pack.equals("OKGO"))
+          if (total >= fileSize || received.getLength() <= 9)
           {
-            System.out.println("good to go");
-            DatagramPacket fileName = new DatagramPacket(filename.getBytes(),
-                filename.getBytes().length, InetAddress.getByName(ip),
-                PORT);
-            socket.send(fileName);
-          }
-          else
-            continue;
-
-          socket.receive(received);
-          pack = new String(received.getData(),0,received.getLength());
-          System.out.println(pack);
-          if (!pack.equals("Valid"))
-          {
-            System.out.println(pack);
-            continue;
-          }
-          System.out.println(pack);
-          String[] splitName = filename.split("/");
-          File newFile = new File("lcaol_" + 
-              splitName[splitName.length -1]);
-          if (newFile.exists())
-          {
-            System.out.println("Error: can't write to disk");
-            continue;
-          }
-
-          newFile.createNewFile();
-
-          FileOutputStream fos = new FileOutputStream(newFile);
-
-          while (true)
-          {
-            socket.receive(received);
-            if (received.getLength() <= 10)
+            System.out.println(total >= fileSize);
+            cmd = new String(received.getData(), 0,
+                received.getLength());
+            if (cmd.equals("END_FILE") || total >= fileSize)
             {
-              cmd = new String(received.getData(), 0,
-                  received.getLength());
-              if (cmd.equals("END_FILE"))
-              {
-                System.out.println("received file");
-                break;
-              }
-            }  
-            fos.write(received.getData(), 0, received.getLength());
+              System.out.println("received file:"+received.getLength());
+              break;
+            }
           }
-      }
+          total += (long) received.getLength();
+         System.out.println(total + " filesize: " + Long.parseLong(pack));
+        if (((received.getLength() / totalLeft)) >= 1)
+        { 
+          System.out.println(totalLeft + " left gotten: " + received.getLength());
+          fos.write(received.getData(), 0, (int) totalLeft);
+          
+        }
+        else
+          fos.write(received.getData(), 0, received.getLength());
+        totalLeft -= received.getLength();
+        }
     }
-
-    /**DatagramPacket packet = new DatagramPacket(buffer, buffer.length, 
-        InetAddress.getByName(args[0]),PORT);
-    */
-    /**socket.send(packet);
-    Date timeSent = new Date();
-    socket.receive(packet);
-    Date timeReceived = new Date();
-
-    System.out.println("" + (timeReceived.getTime()-timeSent.getTime()) + 
-        " ms " + new String(packet.getData(),0,packet.getLength()));
-    */
   }
 }
--- udp-server/EchoServer.java
+++ udp-server/EchoServer.java
@@ -6,7 +6,7 @@ import java.nio.*;
 public class EchoServer {
 
   public static final int PORT = 4444;
-  public static final int BUFFER = 512;
+  public static final int BUFFER = 1024;
 
   public static void main(String args[] ) throws Exception
   {
@@ -18,19 +18,13 @@ public class EchoServer {
     {
       socket.receive(packet);
       String received = new String(packet.getData(), 0, packet.getLength());
-      if (received.equals("hi"))
+      if (received.equals("GET_FILE"))
       {
-        System.out.println("Connected to:  " + packet.getAddress() +
+        System.out.println("Request from:  " + packet.getAddress() +
             " at port: " + packet.getPort()); 
-        String reply = "what?";
-        DatagramPacket repPacket = new DatagramPacket(reply.getBytes(),
-            reply.getBytes().length, packet.getAddress(), packet.getPort());
-        socket.send(repPacket);
         run(socket, packet);
       }
-      
     }
-
   }
 
   public static void run(DatagramSocket sock, DatagramPacket pack) 
@@ -40,70 +34,63 @@ public class EchoServer {
     DatagramPacket packet = pack;
     boolean validFile = false;
     boolean ok = false;
+    String request = "";
 
-    while (true)
+    String ready = "OKGO";
+    DatagramPacket okgo = new DatagramPacket(ready.getBytes(),
+          ready.getBytes().length, packet.getAddress(),
+          packet.getPort());
+    socket.send(okgo);
+
+    socket.receive(packet);
+    request = new String(packet.getData(), 0, packet.getLength());
+    System.out.println("Filename: " + request);
+    File file = new File(request);
+    String replyStr = "";
+    if (!file.exists())
+      replyStr = "Error: file doesn't exist.";
+    else if (!file.isFile())
+      replyStr = "Error: not a file";
+    else if (!file.canRead())
+      replyStr = "Error: can't read from file.";
+    else
     {
-      socket.receive(packet);
-      String request = new String(packet.getData(), 0, packet.getLength());
-      if (request.equals("GetFile") && ok == false)
-      {
-        String ready = "OKGO";
-        DatagramPacket okgo = new DatagramPacket(ready.getBytes(),
-            ready.getBytes().length, packet.getAddress(),
-            packet.getPort());
-        socket.send(okgo);
-        ok = true;
-      }
-      System.out.println(ok + request);
-      if (ok == true )
-      {
-        socket.receive(packet);
-        request = new String(packet.getData(), 0, packet.getLength());
-        System.out.println("Filename: " + request);
-        File file = new File(request);
-        String replyStr = "";
-        if (!file.exists())
-          replyStr = "Error: file doesn't exist.";
-        else if (!file.isFile())
-          replyStr = "Error: not a file";
-        else if (!file.canRead())
-          replyStr = "Error: can't read from file.";
-        else
-        {
-          replyStr = "Valid";
-          validFile = true;
-        }
-        DatagramPacket error = new DatagramPacket(replyStr.getBytes(),
-            replyStr.getBytes().length, packet.getAddress(),
-            packet.getPort());
-        socket.send(error);
-        if (!validFile)  
-          return;
-
-        FileInputStream fin = new FileInputStream(file);
-        DatagramPacket filePack;
-
-        int size = 0;
-        byte[] buffer = new byte[BUFFER];
-        ByteBuffer bb = ByteBuffer.allocate(4);
-        bb.order(ByteOrder.BIG_ENDIAN);
-        
-        
-        System.out.println("Start transfer..");        
-        while ((size = fin.read(buffer)) > 0)
-        {
-          filePack = new DatagramPacket(buffer, buffer.length,
-              packet.getAddress(), packet.getPort());
-          socket.send(filePack);
-        }
-        System.out.println("Finished transfer");
-        String done = "END_FILE";
-        filePack = new DatagramPacket(done.getBytes(),
-            done.getBytes().length,packet.getAddress(),
-            packet.getPort());
-        socket.send(filePack);
-      }
+      replyStr = Long.toString(file.length());
+      validFile = true;
     }
-  }
+    DatagramPacket error = new DatagramPacket(replyStr.getBytes(),
+        replyStr.getBytes().length, packet.getAddress(),
+        packet.getPort());
+    socket.send(error);
+    if (!validFile)
+    {  
+      System.out.println("File name invalid!!");
+      return;
+    }
+    FileInputStream fin = new FileInputStream(file);
+    DatagramPacket filePack;
 
+    int size = 0;
+    byte[] buffer = new byte[BUFFER];
+    ByteBuffer bb = ByteBuffer.allocate(4);
+    bb.order(ByteOrder.BIG_ENDIAN);
+        
+    System.out.println("Start transfer..");
+    int total = 0;    
+    while ((size = fin.read(buffer)) > 0)
+    {
+      total += size;
+      System.out.println(total);
+      filePack = new DatagramPacket(buffer, buffer.length,
+          packet.getAddress(), packet.getPort());
+      socket.send(filePack);
+    }
+    System.out.println("Finished transfer");
+    String done = "END_FILE";
+    filePack = new DatagramPacket(done.getBytes(),
+        done.getBytes().length,packet.getAddress(),
+        packet.getPort());
+    socket.send(filePack);
+    return; 
+  }
 }
