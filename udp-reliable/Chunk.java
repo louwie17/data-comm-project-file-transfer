929843284808ff8871845863fcd4d173799335a1
Daniel <polaris8920@gmail.com>
2012-11-12 14:00:14 -0400

    Fixed indenting for Chunk

--- udp-reliable/Chunk.java
+++ udp-reliable/Chunk.java
@@ -1,12 +1,12 @@
 public class Chunk
 {
     // First byte is sequence number last 9 bytes are the checksum/CRC
-    // the rest is the actual data 
+    // the rest is the actual data
     private byte[] data;
 
     public Chunk(byte[] bytes, int num)
     {
-    //creates the data byte array with sequence number and checksum    
+        //creates the data byte array with sequence number and checksum
     }
 
     public byte[] getData()
