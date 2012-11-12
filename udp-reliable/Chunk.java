f33ac5b10cf71287b6e42d9f1fca358eca22376e
Lourens <lourensschep@gmail.com>
2012-11-12 16:10:12 -0400

    got the Chunk class working for now

--- udp-reliable/Chunk.java
+++ udp-reliable/Chunk.java
@@ -4,33 +4,35 @@ import java.util.zip.CRC32;
 
 public class Chunk
 {
-    // First byte is sequence number last 9 bytes are the checksum/CRC
+    // First 4 bytes is sequence number last 9 bytes are the checksum/CRC
     // the rest is the actual data
     private byte[] data;
 
     // this one is server side
     public Chunk(byte[] bytes, int num)
     {
-        if (bytes.length == 1015)
+        if (bytes.length == 1011)
         {
             ByteBuffer buffer = ByteBuffer.allocate(1024);
             buffer.putInt(num);
             buffer.put(bytes);
 
+            byte[] sum = buffer.array();
             Checksum crc = new CRC32();
-            crc.update(bytes, 0, bytes.length);
-
+            crc.update(sum, 0, 1015);
             buffer.putLong(crc.getValue());
 
-            this.data = buffer.array();
+            data = buffer.array();
         }
+        else
+            System.out.println("wrong size");
         //creates the data byte array with sequence number and checksum
     }
 
     // this one is client side
     public Chunk(byte[] bytes)
     {
-        this.data = bytes;
+        data = bytes;
     }
 
     // for the server side
@@ -42,21 +44,23 @@ public class Chunk
     // Check the CRC on the client side
     public boolean checkCRC()
     {
-        ByteBuffer bb = ByteBuffer.allocate(8);
-        bb.get(data, 1016, data.length);
-
+        ByteBuffer bb = ByteBuffer.allocate(1024);
+        bb.put(data);
+        
         Checksum crc = new CRC32();
-        crc.update(data, 1, 1015);
+        crc.update(data, 0, 1015);
 
-        return bb.getLong() == crc.getValue();
+        System.out.println(bb.getLong(1015));
+        System.out.println(crc.getValue());
+        return bb.getLong(1015) == crc.getValue();
     }
 
     // client side
     public int getSequenceNumber()
     {
-        ByteBuffer bb = ByteBuffer.allocate(1);
-        bb.get(data, 0, 1);
-        return bb.getInt();
+        ByteBuffer bb = ByteBuffer.allocate(1024);
+        bb.put(data);
+        return bb.getInt(0);
     }
 
     // for the client side
--- udp-reliable/test.java
+++ udp-reliable/test.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.lang.StringBuffer;
+import java.util.Arrays;
 
 public class test
 {
@@ -6,16 +8,24 @@ public class test
     public static void main(String[] args)
     {
         Scanner s = new Scanner(System.in);
-        System.out.print("Test Line: ");
-        String input = s.nextLine();
+        System.out.print("Enter Char: ");
+        String input = s.nextLine();        
+        StringBuffer test = new StringBuffer(1011);
 
-        byte[] bytes = input.getBytes();
+        while (test.length() < 1011)
+        {
+            test.append(input);
+        }
+        byte[] bytes = test.toString().getBytes();
+
         System.out.println("bytes length: " + bytes.length);
         Chunk server = new Chunk(bytes, 0);
 
         byte[] bytesReceived = server.getPacket();
-    
+        System.out.println("packet Length: " + bytesReceived.length); 
         Chunk client = new Chunk(bytesReceived);
+        System.out.println(Arrays.equals(server.getPacket(), 
+                    client.getPacket()));
 
         System.out.println("Check CRC: " + client.checkCRC());
         System.out.println("Sequence num: " + client.getSequenceNumber());
