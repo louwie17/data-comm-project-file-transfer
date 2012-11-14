86204fb640891c9124987b9712f85103052c3ca2
daniel <daniel@daniel-VirtualBox.(none)>
2012-11-14 11:43:28 -0400

    Added javadoc comments to IChunkHandler and IChunkSupplier.

--- udp-reliable/IChunkHandler.java
+++ udp-reliable/IChunkHandler.java
@@ -1,5 +1,16 @@
+/**
+ * An interface for receiving data from the reliable protocol.
+ */
 public interface IChunkHandler
 {
+    /**
+     * Handles a new chunk of data from the protocol.
+     * @param data the chunk containing the payload
+     */
     public void receiveData(Chunk data);
+
+    /**
+     * Called when the protocol reports end-of-file.
+     */
     public void finish();
 }
--- udp-reliable/IChunkSupplier.java
+++ udp-reliable/IChunkSupplier.java
@@ -1,5 +1,12 @@
+/**
+ * An interface for supplying data to the reliable protocol.
+ */
 public interface IChunkSupplier
 {
-    // gives next chunk or null if none left
+    /**
+     * Supplies data to the protocol.  Returns the next chunk of data or null
+     * if no more data exists.
+     * @return the next chunk of data or null
+     */
     public Chunk nextChunk();
 }
